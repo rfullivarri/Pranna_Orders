f939d3a0b543aa48342884b86b565820d9b70b91
rfullivarri <113726007+rfullivarri@users.noreply.github.com>
2023-09-24 12:49:40 +0200

    mm

--- Pranna.py
+++ Pranna.py
@@ -208,12 +208,12 @@ if st.button("Guardar en el Historial"):
     st.success("Los datos han sido guardados en el historial.")
 
 #FILTRO
-    selected_dates = st.multiselect("Filtrar por Cliente",list(df_app['Nombre'].unique()))
-
-    if selected_dates:
-        filtered_df = df_app[df_app['Nombre'].isin(selected_dates)]
-    else:
-        filtered_df = df_app
+selected_dates = st.multiselect("Filtrar por Cliente",list(df_app['Nombre'].unique()))
+if selected_dates:
+    filtered_df = df_app[df_app['Nombre'].isin(selected_dates)]
+else:
+    filtered_df = df_app
+    
 historial_df = pd.read_csv(r"historial.csv").drop_duplicates()
 st.dataframe(historial_df,use_container_width=True)
 
